5db14db4ab78788df887f84d3ff03cd1d3ab9912
victorstorchan <storchan@stanford.edu>
2016-02-24 10:05:42 -0800

    Create henkelMatrix.hpp

---
+++ henkelMatrix.hpp
@@ -0,0 +1,14 @@
+#ifndef ASSIGNMENT_HPP
+#define ASSIGNMENT6_HPP
+
+#include <iostream>
+#include <string>
+#include <math.h>
+#include <cmath>
+#include <boost/multi_array.hpp>
+
+
+void transfo_henkel(boost::multi_array<int,2>  &input);
+
+
+#endif /* ASSIGNMENT_HPP */
